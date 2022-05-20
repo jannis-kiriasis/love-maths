f22764df912905463313a296fbf2761591c5ebcc
Jannis Kiriasis <jannis.kiriasis@gmail.com>
2022-05-20 14:34:28 +0000

    Set up structure for game functions

---
+++ assets/js/script.js
@@ -0,0 +1,32 @@
+function runGame() {
+
+}
+
+function checkanswer() {
+
+}
+
+function calculateCorrectAnswer() {
+
+}
+
+function incrementScore() {
+
+}
+
+function incrementWrongAnswer() {
+
+}
+
+function displayAdditionQuestion() {
+
+}
+
+function displaySubtractQuestion() {
+
+}
+
+function displayMultiplyQuesiton() {
+
+}
+
