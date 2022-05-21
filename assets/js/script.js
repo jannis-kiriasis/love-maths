6259de6866ddb2923d01462fcdbe79b112011411
Jannis Kiriasis <jannis.kiriasis@gmail.com>
2022-05-21 08:31:21 +0000

    Display addition question to the user

--- assets/js/script.js
+++ assets/js/script.js
@@ -13,20 +13,29 @@ document.addEventListener("DOMContentLoaded", function(){
                 alert("You clicked submit!");
             } else {
                 let gameType = this.getAttribute("data-type");
-                alert(`You clicked ${gameType}`);
+                runGame(gameType);
             }
         })
     }
+
+    runGame("addition");
 })
 
 /**
  * The mai game "loop", called when the script is first loaded 
  * and after the user's answer has been processed
  */
-function runGame() {
+function runGame(gameType) {
     //Create 2 random numbers between 1 and 25
     let num1 = Math.floor(Math.random() * 25) + 1;
     let num2 = Math.floor(Math.random() * 25) + 1;
+
+    if (gameType === "addition") {
+        displayAdditionQuestion(num1, num2);
+    } else {
+        alert(`unknown game type: ${gameType}`);
+        throw `unknown game type: ${gameType}. Aborting!`;
+    }
 }
 
 function checkanswer() {
@@ -45,17 +54,23 @@ function incrementWrongAnswer() {
 
 }
 
-function displayAdditionQuestion() {
-
+function displayAdditionQuestion(operand1, operand2) {
+    document.getElementById('operand1').textContent = operand1;
+    document.getElementById('operand2').textContent = operand2;
+    document.getElementById('operator').textContent = '+';
 }
 
 
 
 function displaySubtractQuestion() {
-
+    document.getElementById('operand1').textContent = operand1;
+    document.getElementById('operand2').textContent = operand2;
+    document.getElementById('operator').textContent = '-';
 }
 
 function displayMultiplyQuesiton() {
-
+    document.getElementById('operand1').textContent = operand1;
+    document.getElementById('operand2').textContent = operand2;
+    document.getElementById('operator').textContent = '*';
 }
 
